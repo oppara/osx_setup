0fc7c29abc7f89037d9d21730d3a76f928b35363
oppara <oppara@oppara.tv>
2017-02-05 20:03:28 +0900

    Add setup vagrant script

---
+++ setup.d/vagrant.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eu
+
+if ! type vagrant > /dev/null 2>&1; then
+  brew cask install vagrant
+fi
+
+vagrant plugin install dotenv
+vagrant plugin install vagrant-cachier
+vagrant plugin install vagrant-vbguest
+vagrant plugin install vagrant-aws
+vagrant plugin install vagrant-docker-compose
+# vagrant plugin install vagrant-omnibus
+# vagrant plugin install vagrant-berkshelf
