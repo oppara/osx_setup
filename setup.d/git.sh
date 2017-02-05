e45ebcdecb6a13cfa944df9fa91b8e314af8a633
oppara <oppara@oppara.tv>
2017-02-05 20:11:01 +0900

    Add setup git script

---
+++ setup.d/git.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eu
+
+src="/usr/local/share/git-core/contrib/diff-highlight/diff-highlight"
+target="/usr/local/bin/diff-highlight"
+if [ -f "${src}" -a ! -L "${target}" ]; then
+  ln -s "${src}" "${target}"
+fi
