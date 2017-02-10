20c5b1c5821939885a28f2176fb5add4de5a4bb0
oppara <oppara@oppara.tv>
2017-02-11 01:24:04 +0900

    Undecorate script

--- setup.d/go.sh
+++ setup.d/go.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 
-comands() {
-  cat << EOF
-revealgo github.com/yusukebe/revealgo/cmd/revealgo
-memo github.com/mattn/memo
-EOF
-}
-
-install() {
-  input="$(cat)"
-  echo "${input}" |
-  while read -r line
-  do
-    cmd=$(echo "${line}" | awk '{ print $1 }')
-    url=$(echo "${line}" | awk '{ print $2 }')
-    if ! type "${cmd}" > /dev/null 2>&1; then
-      echo "go get ${url}"
-      go get "${url}"
-    fi
-  done
-}
-
-comands | install
+go get github.com/yusukebe/revealgo/cmd/revealgo
+go get github.com/mattn/memo
