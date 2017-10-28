9626e924103e365d5255ec89c6f96e7f74e4e90a
oppara <oppara@oppara.tv>
2017-10-28 19:57:02 +0900

    Fix go setup script

--- setup.d/go.sh
+++ setup.d/go.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-go get github.com/yusukebe/revealgo/cmd/revealgo
-go get github.com/mattn/memo
+go get github.com/motemen/ghq
+# go get github.com/yusukebe/revealgo/cmd/revealgo
+# go get github.com/mattn/memo
+# go get github.com/b4b4r07/gist
