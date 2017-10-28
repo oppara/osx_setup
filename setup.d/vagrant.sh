0f33145821e7b751f4ba18b02090a8ec6bb9fa28
oppara <oppara@oppara.tv>
2017-10-28 20:00:25 +0900

    Fix vagrant setup script

--- setup.d/vagrant.sh
+++ setup.d/vagrant.sh
@@ -5,7 +5,10 @@ if ! type vagrant > /dev/null 2>&1; then
   brew cask install vagrant
 fi
 
+# dotenvを利用して環境ごとでVagrantfileの設定値を変更してみる
+# http://blog.glidenote.com/blog/2014/02/26/vagrant-dotenv/
 vagrant plugin install dotenv
+
 vagrant plugin install vagrant-cachier
 vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-aws
