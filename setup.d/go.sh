caa999a1b61116e4dc757020baa0da00a323aaca
oppara <oppara@oppara.tv>
2018-07-11 01:06:36 +0900

    Fix go path

--- setup.d/go.sh
+++ setup.d/go.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -eu
 
+bin="${HOME}/bin"
+if [ ! -d "${bin}" ]; then
+  git clone git@github.com:oppara/bin.git "${bin}"
+fi
+unset bin
+
+golibexec="/usr/local/opt/go/libexec"
+PATH="$PATH:${golibexec}/bin"
+export GOPATH="${HOME}"
+export GOROOT=${golibexec}
+unset golibexec
+
 go get github.com/motemen/ghq
 go get github.com/peco/migemogrep
 # go get github.com/yusukebe/revealgo/cmd/revealgo
