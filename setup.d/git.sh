8a22af340551e23fc6a5740686cb5e354a6dcdd9
oppara <oppara@oppara.tv>
2022-06-11 11:25:42 +0900

    config(git): fix path

--- setup.d/git.sh
+++ setup.d/git.sh
@@ -3,7 +3,7 @@ set -eu
 
 for cmd in "diff-highlight" \
   "git-jump"; do
-  src="/usr/local/share/git-core/contrib/${cmd}/${cmd}"
+  src="/opt/homebrew/share/git-core/contrib/${cmd}/${cmd}"
   target="/usr/local/bin/${cmd}"
   if [ -f "${src}" -a ! -L "${target}" ]; then
     ln -s "${src}" "${target}"
