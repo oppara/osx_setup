0c32c1fc249c5ca564c296e5fa64a62315656d9a
oppara <oppara@oppara.tv>
2022-06-12 21:26:17 +0900

    fix(golang): fix setup script

--- setup.d/go.sh
+++ setup.d/go.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 
-bin="${HOME}/bin"
-if [ ! -d "${bin}" ]; then
-  git clone git@github.com:oppara/bin.git "${bin}"
-fi
-unset bin
 
-golibexec="/usr/local/opt/go/libexec"
+golibexec="/opt/homebrew/opt/go/libexec"
 PATH="$PATH:${golibexec}/bin"
 export GOPATH="${HOME}"
 export GOROOT=${golibexec}
 unset golibexec
 
-go get github.com/mattn/memo
-go get github.com/motemen/ghq
-# go get github.com/yusukebe/revealgo/cmd/revealgo
-# go get github.com/b4b4r07/gist
+go install github.com/mattn/memo@latest
+go install github.com/x-motemen/ghq@latest
+# go install github.com/yusukebe/revealgo/cmd/revealgo
+# go install github.com/b4b4r07/gist
