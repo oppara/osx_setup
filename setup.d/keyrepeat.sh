add6b5ee68665b64c661997946e55da88f14053c
oppara <oppara@oppara.tv>
2023-06-20 23:10:07 +0900

    feat(defaults): keyrepeat for vscode

---
+++ setup.d/keyrepeat.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
