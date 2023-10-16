ae47f42fb0c2a74d464aa95544a2cd0f4b01ace7
oppara <oppara@oppara.tv>
2023-10-16 23:53:13 +0900

    fix(gh): handle error

--- setup.d/gh.sh
+++ setup.d/gh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Ceu
 
-gh extension install https://github.com/yusukebe/gh-markdown-preview
+gh extension install https://github.com/yusukebe/gh-markdown-preview || true
 
-gh extension install https://github.com/cappyzawa/gh-ghq-cd
-gh alias set cd ghq-cd
+gh extension install https://github.com/cappyzawa/gh-ghq-cd || true
+gh alias set cd ghq-cd --clobber
